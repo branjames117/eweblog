1c72df357eb94f7d571ddbaec02470d096149ec1
Brandon Hoskins <branjames117@gmail.com>
2022-01-09 18:32:32 -0600

    configred server

---
+++ db/schema.sql
@@ -0,0 +1,3 @@
+DROP DATABASE IF EXISTS eweblog_db;
+
+CREATE DATABASE eweblog_db;
--- server.js
+++ server.js
@@ -1,17 +1,47 @@
 const express = require('express');
+const exphbs = require('express-handlebars');
+const session = require('express-session');
 const path = require('path');
+const routes = require('./controllers');
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
+// initialize app
 const app = express();
+
+// tell app to use session middleware
+app.use(
+  session({
+    secret: process.env.SECRET,
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({ db: sequelize }),
+  })
+);
+
 const PORT = process.env.PORT || 3001;
 
+// tell app to use handlebars for its view engine
+const hbs = exphbs.create({});
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+// boilerplate app middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', (req, res) => {
-  res.send(`<script src="/js/script.js"></script>`);
-});
+// tell app to use our custom routes
+app.use(routes);
 
-app.listen(3001, () => {
-  console.log(`Server listening at http://localhost:${PORT}.`);
+// sync sequelize with db before telling server to listen
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => {
+    if (!process.env.PORT) {
+      console.log(`Server listening at http://localhost:${PORT}.`);
+    } else {
+      console.log(`Server listening on port ${PORT}`);
+    }
+  });
 });
